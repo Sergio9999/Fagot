55899d732116e544f89b03b6a835e45a3afe6486
Sergey Sobolev <Sergrpm@mail.ru>
2016-06-28 19:11:28 +0300

    Change Footer

--- public/js/myJS.js
+++ public/js/myJS.js
@@ -1,11 +1,11 @@
  $(window).ready(function() {
   function logoPosition(){
     var widthLogo = $('#logo').width();
-    var widthFooter = $(window).width();
-    leftLogo = (widthFooter - widthLogo)/2;
+    var widthFooter = $(window).width(); //Ширина окна
+    var leftLogo = (widthFooter - widthLogo)/2;
     var heightLogo = $('#logo').height();
     var heightFooter = $('.site-footer').height();
-    topLogo = (heightFooter - heightLogo)/2 + 20;
+    var topLogo = (heightFooter - heightLogo)/2 + 20;
   $('#logo').css({left:leftLogo, top: topLogo});
 };
 logoPosition();
