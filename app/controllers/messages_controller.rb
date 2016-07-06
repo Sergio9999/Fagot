56ecebbdad43fb9bf824875505cef0b6149bbb52
Sergey Sobolev <Sergrpm@mail.ru>
2016-07-06 17:14:41 +0300

    Finish Callback

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,7 @@ class MessagesController < ApplicationController
     name = params[:name]
     phone = params[:phone]
     comment = params[:comment]
-    InviterMailer.callback_information(:name, :phone, :comment).deliver_now
+    InviterMailer.callback_information(name, phone, comment).deliver_now
     redirect_to root_path
   end
 end
