5db29f31ad61fd454cbb24c7553cb0f82e266e55
Sergey Sobolev <Sergrpm@mail.ru>
2016-07-18 23:23:11 +0300

    change mail for frolkin and negrebetskaya

--- app/mailers/inviter_mailer.rb
+++ app/mailers/inviter_mailer.rb
@@ -1,8 +1,8 @@
 class InviterMailer < ApplicationMailer
   def callback_information(message)
     @message = message
-    mail_first = 'sergrpm@mail.ru'
-    mail_second = 'sobolevsergey9999@gmail.com'
+    mail_first = 'flp.Frolkin@bk.ru'
+    mail_second = 's.negrebmail@gmail.com'
     mail to: [mail_first, mail_second], subject: 'Добрый день'
   end
 end
