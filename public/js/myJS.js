34ac8848c501aa7d7f30d582b8c4a8923ba1f832
Sergey Sobolev <Sergrpm@mail.ru>
2016-07-20 17:49:47 +0300

    Add little style to header

--- public/js/myJS.js
+++ public/js/myJS.js
@@ -18,7 +18,27 @@ $('#products button.price').click(function(){
 $('#products .price-list button.fade_button').click(function(){
   $('#products .price-list').fadeOut('slow');
 });
-});
+
+$(document).on('scroll', function () {
+        // if the scroll distance is greater than 100px
+        if ($(window).scrollTop() > 200) {
+          // do something
+          $('.site-header').addClass('scrolled-header');
+          $('#templatemo_logo').css({
+            'background-image':'url(../images/logo2.png)',
+            'height': '82px',
+            'margin-top':'1px'
+          });
+         }
+         else {
+          $('.site-header').removeClass('scrolled-header');
+          $('#templatemo_logo').css({
+            'background-image':'url(../images/logo3.png)',
+            'height': '89px',
+            'margin-top':'5px'
+          });
+         };
+      });
 $(window).load(function(){
  var $page = $('html, body');
  var flash_error = $('#error_explanation');
@@ -26,15 +46,16 @@ $(window).load(function(){
   var scroll_error = flash_error.offset().top;
   $page.animate({
     scrollTop: scroll_error - 100
-  }, 400);
+  }, 800);
  };
  var alert_info = $('.alert.alert-info');
  if (alert_info.length == 1) {
    var scroll_alert_info = alert_info.offset().top;
    $page.animate({
      scrollTop: scroll_alert_info - 150
-   }, 400 )
-  }
+   }, 700 )
+  };
+ })
 })
 
 
