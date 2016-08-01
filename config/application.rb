2fe0faef339b6acadc2c82921a91bf1220889f15
Sergey Sobolev <Sergrpm@mail.ru>
2016-08-01 20:05:31 +0300

    add russian style

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Fagot
   class Application < Rails::Application
     Rails.application.secrets.each { |key, value| ENV[key.to_s] ||= value }
     config.active_record.raise_in_transactional_callbacks = true
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', 'models', '*.{rb,yml}')]
     config.i18n.default_locale = :ru
   end
 end
